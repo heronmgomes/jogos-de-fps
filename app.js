3c787734d5cf51d13499107e2f50da42ed2885d5
Heron Menezes Gomes <ullisses.1994@gmail.com>
2025-12-17 02:31:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ function pesquisar() {
   let section = document.getElementById("resultados-pesquisa");
   let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-  section.style.opacity = 0;
-
-setTimeout(() => {
-    section.innerHTML = resultados;
-    section.style.opacity = 1;
-}, 150);
-
-
   if (!campoPesquisa) {
     section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um jogo de FPS.</p>";
     return;
