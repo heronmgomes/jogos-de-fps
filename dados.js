eebec99c86ba83a829eb4a89e378bfd2d0505a8a
Heron Menezes Gomes <ullisses.1994@gmail.com>
2025-12-17 00:54:15 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+  {
+    titulo: "Call of Duty",
+    descricao: "Call of Duty é uma franquia de jogos de tiro em primeira pessoa.",
+    link: "https://www.callofduty.com/br/pt/store/games/warzone"
+  },
+  {
+    titulo: "Counter Strike",
+    descricao: "Counter-Strike é um dos jogos de tiro em primeira pessoa mais populares do mundo.",
+    link: "https://www.baixarcs16agora.com/baixar-counter-strike/"
+  },
+  {
+    titulo: "Pubg Mobile",
+    descricao: "PUBG MOBILE é um jogo de tiro que coloca até 100 jogadores em uma ilha.",
+    link: "https://play.google.com/store/apps/details?id=com.tencent.ig"
+  }
+];
