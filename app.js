2e9d28239e2e15e6a92d2b1c01cf62169c1578ab
Heron Menezes Gomes <ullisses.1994@gmail.com>
2025-12-17 00:53:54 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+function pesquisar() {
+  let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um jogo de FPS.</p>";
+    return;
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+  let resultados = "";
+
+  for (let dado of dados) {
+    let titulo = dado.titulo.toLowerCase();
+    let descricao = dado.descricao.toLowerCase();
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+      resultados += `
+        <div class="item-resultado">
+          <h2>${dado.titulo}</h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>
+      `;
+    }
+  }
+
+  section.innerHTML = resultados || "<p>Nada foi encontrado</p>";
+}
